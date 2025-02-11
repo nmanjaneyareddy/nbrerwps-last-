965914ff04a964b46c63d8759dde12fc6544ff81
Anjaneya Reddy (IGIDR, Mumbai) <areddy.ragini@gmail.com>
2025-02-11 12:51:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,9 +56,11 @@ def scrape_nber():
         parts = row['Title'].split(':', maxsplit=1)
         return pd.Series(parts if len(parts) == 2 else [parts[0], ''], index=['Title1', 'Subtitle'])
     
-    split_titles = df.apply(safe_split, axis=1)
-    df = pd.concat([df, split_titles], axis=1)
-    df.drop('Title', axis=1, inplace=True)
+    if 'Title' in df.columns:
+        split_titles = df.apply(safe_split, axis=1)
+        df = pd.concat([df, split_titles], axis=1)
+        df.drop('Title', axis=1, inplace=True)
+    
     return df
 
 def convert_df_to_excel(df):
